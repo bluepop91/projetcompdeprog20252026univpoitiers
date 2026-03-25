b482a7ebb8206199ce198a63333f58d26b9ff148
Yohann <yoyovedie91@gmail.com>
2026-03-25 15:38:39 +0100

    Sauvegarde iteration2

---
+++ Iteration 2/Tetris/NoyauTetris/NoyauTetris.cs
@@ -0,0 +1,35 @@
+﻿namespace NoyauTetris;
+
+public enum Couleur
+{
+    Bleu,
+    Rose,
+    Violet,
+    Blanc,
+    Noir
+    
+}
+public class JeuTetris
+{
+    public int LargeurGrille;
+    public int HauteurGrille;
+
+    public JeuTetris()
+    {
+        LargeurGrille = 12;
+        HauteurGrille = 15;
+    }
+
+}
+
+public class Position
+{
+    public int X;
+    public int Y;
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
